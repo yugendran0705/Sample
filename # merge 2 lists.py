23c0d2461fc3062ad3b379aefa454ac51135d88f
Yugendran <108662548+yugendran0705@users.noreply.github.com>
2022-07-09 20:00:03 +0530

    2 new programs
    done

---
+++ # merge 2 lists.py
@@ -0,0 +1,23 @@
+# merge 2 lists sorted
+
+def merge(l1, l2):
+    l3 = []
+    i = 0
+    j = 0
+    while i < len(l1) and j < len(l2):
+        if l1[i] < l2[j]:
+            l3.append(l1[i])
+            i += 1
+        else:
+            l3.append(l2[j])
+            j += 1
+    if i == len(l1):
+        l3.extend(l2[j:])
+    else:
+        l3.extend(l1[i:])
+    return l3
+l1=[1,2,3,4,5]
+l2=[6,7,8,9,10]
+l3=merge(l1,l2)
+print(l3)
+
---
+++ # sorting given list of strings by lengt.py
@@ -0,0 +1,9 @@
+# sorting given list of strings by length of string
+
+def sort_by_length(lst):
+    lst.sort(key=len)
+    return lst
+
+a=['a','bb','ccc','dddd','eeeee']
+print(sort_by_length(a))
+# ['a', 'bb', 'ccc', 'dddd', 'eeeee']
